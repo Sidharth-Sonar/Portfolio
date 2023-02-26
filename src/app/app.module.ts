e15bbf08fba8a93873df02d2091b7c4ac838101f
Sidharth Sonar <Sidharthsonar1111@gmail.com>
2023-02-26 16:57:52 +0530

    created base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { PortComponent } from './port/port.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    PortComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
